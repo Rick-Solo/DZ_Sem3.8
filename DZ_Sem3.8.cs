f42efc2e1b77188b79b042c8ccec90e417e9716e
Rick Solo <mrfox7398@mail.ru>
2022-09-16 13:45:31 +0300

    Zadacha 3

--- DZ_Sem3.8.cs
+++ DZ_Sem3.8.cs
@@ -62,21 +62,20 @@
 
 // Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
 
-int[,] array = new int[4,4];
-int[] sum = new int[array.GetLength(1)];
-for (int i = 0; i < array.GetLength(0); i++)
-{
-    
-    for (int j = 0; j < array.GetLength(1); j++)
-    {
-        array[i, j] = new Random().Next(0, 10);
-        Console.Write(array[i, j] + "\t");
-        sum[i] += array[i,j];
-    }
-    Console.WriteLine();
-    // Console.WriteLine(sum[i]);     // <- Включить для проверки (суммы)
-}
-Console.WriteLine("\n"+"Номер строки с наименьшей суммой элементов: "+ (Array.IndexOf(sum, sum.Min())+1));
+// int[,] array = new int[4,4];
+// int[] sum = new int[array.GetLength(1)];
+// for (int i = 0; i < array.GetLength(0); i++)
+// {
+//     for (int j = 0; j < array.GetLength(1); j++)
+//     {
+//         array[i, j] = new Random().Next(0, 10);
+//         Console.Write(array[i, j] + "\t");
+//         sum[i] += array[i,j];
+//     }
+//     Console.WriteLine();
+//     // Console.WriteLine(sum[i]);     // <- Включить для проверки (суммы)
+// }
+// Console.WriteLine("\n"+"Номер строки с наименьшей суммой элементов: "+ (Array.IndexOf(sum, sum.Min())+1));
 
 // ------------------------------------------------
 // Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
@@ -87,6 +86,53 @@ Console.WriteLine("\n"+"Номер строки с наименьшей сумм
 // 18 20
 // 15 18
 
+Console.Write("Введите количество строк 1 матрицы: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов 1 матрицы: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("\n" + "Первая матрица:");
+
+int[,] matrix1 = new int[m, n];
+for (int i = 0; i < matrix1.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix1.GetLength(1); j++)
+    {
+        matrix1[i, j] = new Random().Next(10);
+        Console.Write(matrix1[i,j] + "\t");
+    }
+    Console.WriteLine();
+}
+
+Console.WriteLine("\n" + "Вторая матрица:");
+
+int[,] matrix2 = new int[n, m];
+for (int i = 0; i < matrix2.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix2.GetLength(1); j++)
+    {
+        matrix2[i, j] = new Random().Next(10);
+        Console.Write(matrix2[i,j] + "\t");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine("\n" + "Результат произведения матриц:");
+int[,] matrixOtvet = new int[matrix1.GetLength(0), matrix2.GetLength(1)];
+
+for (int i = 0; i < matrix1.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix2.GetLength(1); j++)
+    {
+        for (int k = 0; k < matrix2.GetLength(0); k++)
+        {
+            matrixOtvet[i,j] += matrix1[i,k] * matrix2[k, j];
+        }
+        Console.Write(matrixOtvet[i,j] + "\t");
+    }
+    Console.WriteLine();
+}
+
+// ---------------------------------------------------------
 // Задача 60. ...Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
 // Массив размером 2 x 2 x 2
 // 66(0,0,0) 25(0,1,0)
